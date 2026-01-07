f75881e6fc3bc7f0554cc7027a361c01985579ed
rchahla <rchahla2@uwo.ca>
2026-01-06 21:28:50 -0500

    Finished creating the banner on the home page which has a carousel that you can scroll through with different image banners

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,24 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
       <Header />
-      {/* Banner */}
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+
+        <Banner />
+
+        {/* ProductFeed */}
+        <ProductFeed />
+      </main>
     </div>
   );
 }
