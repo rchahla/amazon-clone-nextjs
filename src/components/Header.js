1e978f7a7f943d9f72cc77d2d4ceb04dfe8bb6d9
rchahla <rchahla2@uwo.ca>
2026-01-06 20:54:52 -0500

    Created the bottom portion of the navigation bar therefore completing the page header

--- src/components/Header.js
+++ src/components/Header.js
@@ -55,7 +55,22 @@ function Header() {
         </div>
       </div>
       {/*Bottom nav*/}
-      <div></div>
+
+      <div className="flex items-center bg-amazon_blue-light text-white text-sm p-2 pl-6 space-x-3">
+        <p className="link flex items-center">
+          <Bars3Icon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
     </header>
   );
 }
